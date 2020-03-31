1c096cc2c7fbd138365d94f72100d62b5a36348f
kvnqwang <62963926+kvnqwang@users.noreply.github.com>
2020-03-31 14:46:53 -0700

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,11 +1,11 @@
+import PyPDF2 #pip install PyPDF2
 def PDFconvert(pdfFile):
-    import PyPDF2 #pip install PyPDF2
     f = open(pdfFile,'rb')
+    a = open('txt.txt', 'w')
     pdfRead = PyPDF2.PdfFileReader(f)
-    pagePDF = pdfRead.getPage(0)
-    a = open('txt.txt','w')
-    a.write(pagePDF.extractText())
+    for i in range(pdfRead.getNumPages()):
+        pagePDF = pdfRead.getPage(i)
+        a = open('txt.txt','a')
+        a.write(pagePDF.extractText())
     f.close()
     a.close()
-
-PDFconvert('helloworld.pdf')
