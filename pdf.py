9731c6210f2fa42e80f87dedcba5c4e025196847
kvnqwang <62963926+kvnqwang@users.noreply.github.com>
2020-03-31 14:53:18 -0700

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,11 +1,23 @@
-import PyPDF2 #pip install PyPDF2
-def PDFconvert(pdfFile):
-    f = open(pdfFile,'rb')
-    a = open('txt.txt', 'w')
+import PyPDF2  # pip install PyPDF2
+
+# write as .txt file
+def PDFtotxt(pdfFile):
+    f = open(pdfFile, 'rb')
     pdfRead = PyPDF2.PdfFileReader(f)
     for i in range(pdfRead.getNumPages()):
         pagePDF = pdfRead.getPage(i)
-        a = open('txt.txt','a')
+        a = open('txt.txt', 'a')
         a.write(pagePDF.extractText())
     f.close()
     a.close()
+
+
+# Python string
+def PDFtoString(pdfFile):
+    f = open(pdfFile, 'rb')
+    a = ''
+    pdfRead = PyPDF2.PdfFileReader(f)
+    for i in range(pdfRead.getNumPages()):
+        pagePDF = pdfRead.getPage(i)
+        a = a + pagePDF.extractText()
+    f.close()
